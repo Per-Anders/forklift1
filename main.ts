796101354997c2fb3421564456cf66defc0b830a
Per-Anders Bjune <45122937+Per-Anders@users.noreply.github.com>
2023-03-22 17:03:54 +0100

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,20 @@ radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         pins.servoWritePin(AnalogPin.P1, 15)
     }
+    if (receivedNumber == 3) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 100)
+    }
+    if (receivedNumber == 4) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 100)
+    }
+    if (receivedNumber == 5) {
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 100)
+    }
+    if (receivedNumber == 6) {
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 100)
+    }
 })
+DFRobotMaqueenPlus.I2CInit()
 basic.forever(function () {
     radio.setGroup(1)
 })
