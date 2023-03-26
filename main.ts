a7fc5f2bf7fb4bea6e4b84ccfa0d02bf94e14e61
Per-Anders Bjune <45122937+Per-Anders@users.noreply.github.com>
2023-03-26 15:46:46 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,32 +1,38 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        pins.servoWritePin(AnalogPin.P1, 0)
-    }
-    if (receivedNumber == 2) {
         pins.servoWritePin(AnalogPin.P1, 100)
     }
+    if (receivedNumber == 2) {
+        pins.servoWritePin(AnalogPin.P1, 0)
+    }
     if (receivedNumber == 3) {
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 100)
+        pins.servoWritePin(AnalogPin.P2, 144)
     }
     if (receivedNumber == 4) {
-        DFRobotMaqueenPlus.mototStop(Motors.ALL)
+        pins.servoWritePin(AnalogPin.P2, 180)
     }
     if (receivedNumber == 5) {
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 100)
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 100)
     }
     if (receivedNumber == 6) {
         DFRobotMaqueenPlus.mototStop(Motors.ALL)
     }
     if (receivedNumber == 7) {
-        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 100)
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 100)
     }
     if (receivedNumber == 8) {
         DFRobotMaqueenPlus.mototStop(Motors.ALL)
     }
     if (receivedNumber == 9) {
-        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 100)
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 100)
     }
     if (receivedNumber == 10) {
+        DFRobotMaqueenPlus.mototStop(Motors.ALL)
+    }
+    if (receivedNumber == 11) {
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 100)
+    }
+    if (receivedNumber == 12) {
         DFRobotMaqueenPlus.mototStop(Motors.ALL)
     }
 })
